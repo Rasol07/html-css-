1ba4e16e3c4602ffb8c43e397fccabde3b0623f0
Rasol07 <poloa0709@gmail.com>
2026-06-08 20:45:09 +0900

    다이어리 완성

--- diary/diary.js
+++ diary/diary.js
@@ -13,6 +13,17 @@ const today = new Date();
 
 const currentDate = document.querySelector('.current-date');
 
+const plusButton = document.querySelector('.plus-button');
+const modal = document.querySelector('.modal-background');
+const enterDiary = document.querySelector('.diary-enter');
+const closeButton = document.querySelector('.diary-close');
+const saveButton = document.querySelector('.diary-save');
+const titleEnter = document.querySelector('.title-enter');
+const contentEnter = document.querySelector('.content-enter');
+
+const diaryContainer = document.querySelector('.diary-container');
+// 다이어리 내용 정리 관련
+
 
 // --- 함수 ---
 function showDate() {
@@ -26,6 +37,34 @@ function showDate() {
     `;
 }
 
+function showDiary() {
+    diaryContainer.innerHTML = ``;
+    // 그러면 해당 키값의 data를 배열로 가져오기
+    let selected = `${selectedDate.year}-${selectedDate.month}-${selectedDate.day}`;
+
+    const diaryData = JSON.parse(localStorage.getItem(selected));
+
+    if(!diaryData) return;
+
+     // 이렇게 안하면 계속 그 diaryData가 중첩해서 보일 거임
+    diaryData.forEach(function(diary) {
+        const diaryContent = `
+            <div class = "diary">
+                <span class = "title">
+                    ${diary.title}
+                </span>
+                <p class = "time">${diary.time}</p>
+                <p class = "diary-content">
+                    ${diary.content}
+                </p>
+            </div>
+        `
+        diaryContainer.insertAdjacentHTML('beforeend',diaryContent);
+    })
+    console.log(selected);
+    console.log(diaryData);
+}
+
 // --- 이벤트 리스너 ---
 calendarDates.addEventListener('click', function(e){
     if(e.target.tagName === 'SPAN') {
@@ -34,15 +73,51 @@ calendarDates.addEventListener('click', function(e){
         selectedDate.month = currentMonth;
         selectedDate.day = parseInt(e.target.textContent.trim());
         
-        console.log(day);
+        console.log(selectedDate.day);
     }
     renderCalendar();
 
     showDate();
+    showDiary();
+})
+
+plusButton.addEventListener('click', function() {
+    modal.style.display = 'block';
+    enterDiary.style.display = 'flex';
+})
+
+modal.addEventListener('click', function() {
+    modal.style.display = 'none';
+    enterDiary.style.display = 'none';
+})
+closeButton.addEventListener('click', function(){
+    modal.style.display = 'none';
+    enterDiary.style.display = 'none';
+})
+
+saveButton.addEventListener('click', function() {
+    // localStorage에 값 저장하는 거.........
+    const dataKey = `${currentYear}-${currentMonth}-${day}`; // 날짜로 key 저장
+    const data = JSON.parse(localStorage.getItem(dataKey)) || []; // 이미 들어가있으면 거기에 추가하게 가져오고 아니면 비어있게 초기화
+
+    const newDiary = {
+        title : titleEnter.value,
+        content : contentEnter.value,
+        time : new Date().toLocaleTimeString()
+    }
+    // localStorage 자체에 들어가 있는 걸 가져와서 뒤에 붙이는 형식
+    data.push(newDiary);
+
+    localStorage.setItem(dataKey, JSON.stringify(data));
+    showDiary();
+
+    modal.style.display = 'none';
+    enterDiary.style.display = 'none';
 })
 
 // --- 업데이트 ---
 
 showDate();
+showDiary();
 
 
